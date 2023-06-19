20e4ecc8878e2f76bc3cbc5d86c82450389c3dd8
julizet <juliazet@gmx.de>
2023-06-19 19:56:04 +0200

    alter trees schema according to Dresden

---
+++ supabase/migrations/20230619175112_drop_columns_dd.sql
@@ -0,0 +1,11 @@
+alter table "public"."trees" drop column "eigentuemer";
+
+alter table "public"."trees" drop column "kennzeich";
+
+alter table "public"."trees" drop column "standalter";
+
+alter table "public"."trees" drop column "type";
+
+alter table "public"."trees" drop column "zusatz";
+
+
---
+++ supabase/migrations/20230619175422_add_column_dd.sql
@@ -0,0 +1,3 @@
+alter table "public"."trees" add column "aend_dat" time without time zone;
+
+
